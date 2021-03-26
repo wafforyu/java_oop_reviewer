d2e4e5cff1db742886693b507acb4b7ac0a5b4ed
wafforyu <72655919+wafforyu@users.noreply.github.com>
2021-03-26 23:31:22 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,10 @@ public class Main {
         //create object instance.
         Shape c1 = new Circle("Meters", "Blue", 2);
 
-        //nilagay ko sa println kasi string yung nirereturn ng getter method ko.
         System.out.println(c1.getColor());
         System.out.println(c1.getUnit());
 
         System.out.println("=======================new attribute values=======================");
-        //setter method kung gusto ko palitan yung color blue or yung units.
         c1.setColor("White");
         System.out.println(c1.getColor());
 
@@ -32,7 +30,6 @@ public class Main {
         Shape anotherSquare = new Square();
         
         //ARRAY OF OBJECTS
-
         Shape[] smallPizza = new Circle[5];
 
         for(int i = 0 ; i < smallPizza.length; i++){
@@ -43,6 +40,5 @@ public class Main {
         for(Shape i : smallPizza){
             System.out.println(i.getColor());
         }
-
     }
 }
