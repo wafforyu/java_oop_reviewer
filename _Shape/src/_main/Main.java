d1d42f6799754b374fdc18cedfb5f9931a7b11a1
wafforyu <wafforyu@gmail.com>
2021-04-22 10:54:38 +0800

    organize shape oop reviewer

---
+++ _Shape/src/_main/Main.java
@@ -0,0 +1,48 @@
+package _main;
+import shape.Shape;
+import circle.Circle;
+import square.Square;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        //create object instance.
+        Shape c1 = new Circle("Meters", "Blue", 2);
+
+        System.out.println(c1.getColor());
+        System.out.println(c1.getUnit());
+
+        System.out.println("=======================new attribute values=======================");
+        c1.setColor("White");
+        System.out.println(c1.getColor());
+
+        c1.setUnit("Centimeters");
+        System.out.println(c1.getUnit());
+
+        System.out.println("=============constructor overloading/no arguments used=================");
+        Shape anotherOne = new Circle();
+        System.out.println(anotherOne.getColor());
+
+        System.out.println("The area of c1: " + c1.getArea());
+
+        Shape squarePizza = new Square("meters","yellow",12,3);
+        System.out.println(squarePizza.getArea());
+
+        //Shape aCircle = new Circle("meters", "blue", 2);
+        //Shape aSquare = new Square("millimeters","yellow",5,5);
+        //Shape anotherCircle = new Circle();
+        //Shape anotherSquare = new Square();
+        
+        //ARRAY OF OBJECTS
+        Shape[] smallPizza = new Circle[5];
+
+        for(int i = 0 ; i < smallPizza.length; i++){
+            smallPizza[i] = new Circle("inches", "yellow", i+3); 
+            System.out.println(smallPizza[i].getArea());
+        }
+
+        for(Shape i : smallPizza){
+            System.out.println(i.getColor());
+        }
+    }
+}
---
+++ _Shape/src/square/Square.java
@@ -0,0 +1,23 @@
+package square;
+import shape.Shape;
+
+public class Square extends Shape{
+
+    private double length, width;
+
+    public Square(String unitOfMeasurement, String color, double length, double width){
+        super(unitOfMeasurement,color);
+        this.length = length;
+        this.width = width;
+    }
+
+    public Square(){
+        this.length = 1;
+        this.width = 1;
+    }
+
+    public double getArea() {
+        this.area = length*width;
+        return this.area;
+    }
+}
